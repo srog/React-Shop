b638bfe4201445e11ff248f04a8927135d6d824f
Simon Rogers <simon.rogers@onthebeach.co.uk>
2017-02-10 14:17:25 +0000

    Added customer table, dto and dummy record

---
+++ ReactShop.Core/DTOs/CustomerDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace ReactShop.Core.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ForeName { get; set; }
+        public string Surname { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string Postcode { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
